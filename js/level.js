b7440145f84221d4d5e2d0b7b8925c39df12162c
Maxattax97 <Maxattax97@Gmail.com>
2013-08-05 20:59:49 -0400

    Added Player object with death and jumping.
    Also tweaked trails (for camera) and added touching property to entity.

--- js/level.js
+++ js/level.js
@@ -9,21 +9,28 @@ GameConditions = function() {
         projC.y = 2000;
         projC.vy = -185;
     }
+    if (Player.touching.top == deathBlock || Player.touching.right == deathBlock || Player.touching.left == deathBlock) {
+        Player.die();
+    }
 };
 
 var shoot = false;
 
-new Entity("Ceiling", 3500, 1000, -1000, -1000, Entity.KINEMATIC, "#000000", "#000000");
-new Entity("Wall", 1100, 1500, -1000, -100, Entity.KINEMATIC, "#000000", "#000000");
-new Entity("Platform", 350, 1000, 50, 400, Entity.KINEMATIC, "#000000", "#000000");
-new Entity("Platform", 1450, 1000, 1050, 400, Entity.KINEMATIC, "#000000", "#000000");
-new Entity("Wall", 1100, 200, 1400, 0, Entity.KINEMATIC, "#000000", "#000000");
-new Entity("Wall", 1000, 300, 1500, 100, Entity.KINEMATIC, "#000000", "#000000");
-new Entity("Door", 100, 200, 1400, 200, Entity.PHANTOM, "#222222", "#222222");
+var deathBlock = new Entity("Death Block", 200, 50, 650, 0, Entity.KINEMATIC, "#aa0000");
 
-var projA = new Entity("Projectile A", 100, 100, 550, 1000, Entity.DYNAMIC, "#000000", "#000000");
-var projB = new Entity("Projectile B", 100, 100, 700, 1500, Entity.DYNAMIC, "#000000", "#000000");
-var projC = new Entity("Projectile C", 100, 100, 850, 2000, Entity.DYNAMIC, "#000000", "#000000");
+//var pushBlock = new Entity("Push Block", 80, 80, 250, 50, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.2);
+
+new Entity("Ceiling", 3500, 1000, -1000, -1000, Entity.KINEMATIC);
+new Entity("Wall", 1100, 1500, -1000, -100, Entity.KINEMATIC);
+new Entity("Platform", 350, 1000, 50, 400, Entity.KINEMATIC);
+new Entity("Platform", 1450, 1000, 1050, 400, Entity.KINEMATIC);
+new Entity("Wall", 1100, 200, 1400, 0, Entity.KINEMATIC);
+new Entity("Wall", 1000, 300, 1500, 100, Entity.KINEMATIC);
+new Entity("Door", 100, 200, 1400, 200, Entity.PHANTOM, "#222222");
+
+var projA = new Entity("Projectile A", 100, 100, 550, 1000, Entity.DYNAMIC);
+var projB = new Entity("Projectile B", 100, 100, 700, 1500, Entity.DYNAMIC);
+var projC = new Entity("Projectile C", 100, 100, 850, 2000, Entity.DYNAMIC);
 
 CAMERA.target = Player;
 
--- js/solace.js
+++ js/solace.js
@@ -1,1 +1,109 @@
-var Player = new Entity("Player", 70, 150, 100, 150, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, "textures/Man.png");
+/*
+var Player = {
+    die : function() {
+
+    },
+    jump : function() {
+
+    },
+    move : function(direction) {
+
+    },
+    pushOrPull : function() {
+
+    },
+    name : "Player",
+    width : 70,
+    height : 150,
+    x : 0,
+    y : 0,
+    vx : 0,
+    vy : 0,
+    ax : 0,
+    ay : 0,
+    elasticity : 0.3,
+    friction : 0.4,
+    fillColor : "#000000",
+    outlineColor : "#000000",
+    texture : "textures/Man.png",
+    frame : 0, // For animations
+    type : Entity.DYNAMIC,
+    mass : this.width * this.height,
+    touching : {
+        top: null,
+        left: null,
+        right: null,
+        bottom: null
+    }
+};
+WORKSPACE.push(Player);
+Player.x = 100;
+Player.y = 150;
+*/
+
+var Player = new Entity("Player", 70, 150, 150, 150, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, "textures/Man.png");
+/*    var color = "rgba(0, 0, 0, 0)"; // Invisible
+    color = "rgba(0, 0, 0, 0.5)";
+    Player.head = new Entity("Player Head", 37, 37, Player.x + 16.5, Player.y + 4.5, Entity.DYNAMIC, color, color, 0.3, 0.4);
+    Player.torso = new Entity("Player Torso", 65, 49, Player.x + 2.5, Player.y + 46.5, Entity.DYNAMIC, color, color, 0.3, 0.4);
+    Player.legs = new Entity("Player Legs", 31, 54, Player.x + 19.5, Player.y + 95.5, Entity.DYNAMIC, color, color, 0.3, 0.4);
+    Player.manage = function() {
+        if (Player.head.vx != Player.vx) {
+
+        }
+        Player.head.vx, Player.torso.vx, Player.legs.vx = 0;
+        Player.head.vy, Player.torso.vy, Player.legs.vy = 0;
+
+        Player.head.x = Player.x + 16.5;
+        Player.head.y = Player.y + 4.5;
+
+        Player.torso.x = Player.x + 2.5;
+        Player.torso.y = Player.y + 46.5;
+
+        Player.legs.x = Player.x + 19.5;
+        Player.legs.y = Player.y + 95.5;
+    };
+*/
+    Player.waitingToJump = false;
+    Player.dead = false;
+    Player.jump = function() {
+        if (Player.touching.bottom != null) {
+            Player.vy = -55;
+        }
+        if (Player.touching.bottom == null) {
+            Player.waitingToJump = true;
+        }
+    };
+    Player.move = function(direction) {
+        Player.vx = 50 * direction;
+    };
+    Player.die = function() {
+        if (Player.dead != true) {
+            Player.dead = true;
+            // Fragment the player
+            Player.head1 = new Entity("Player Head", 18.5, 18.5, Player.x + 16.5, Player.y + 4.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.head2 = new Entity("Player Head", 18.5, 18.5, Player.x + 35, Player.y + 4.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.head3 = new Entity("Player Head", 18.5, 18.5, Player.x + 16.5, Player.y + 23, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.head4 = new Entity("Player Head", 18.5, 18.5, Player.x + 35, Player.y + 23, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.torso1 = new Entity("Player Torso", 32.5, 24.5, Player.x + 2.5, Player.y + 46.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.torso2 = new Entity("Player Torso", 32.5, 24.5, Player.x + 35, Player.y + 46.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.torso3 = new Entity("Player Torso", 32.5, 24.5, Player.x + 2.5, Player.y + 71, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.torso4 = new Entity("Player Torso", 32.5, 24.5, Player.x + 35, Player.y + 71, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.legs1 = new Entity("Player Legs", 15.5, 27, Player.x + 19.5, Player.y + 95.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.legs2 = new Entity("Player Legs", 15.5, 27, Player.x + 35, Player.y + 95.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.legs3 = new Entity("Player Legs", 15.5, 27, Player.x + 19.5, Player.y + 122.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+            Player.legs4 = new Entity("Player Legs", 15.5, 27, Player.x + 35, Player.y + 122.5, Entity.DYNAMIC, "#000000", "#000000", 0.3, 0.4, null, Player.vx, Player.vy);
+
+            Player.destroy();
+
+            //CAMERA.target = null;
+            CAMERA.scrollTo(0, 0, 100, Player.head2);
+            wait(5, function() { location.reload() });
+        }
+    };
+
+/*
+var Player = {
+    Player.head = new Entity("Player Head")
+}
+*/
